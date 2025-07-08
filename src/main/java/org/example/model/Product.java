b2e43a798a99f0b247f84cbda714acd06f2b6968
okaymiimi <104463320+okaymiimi@users.noreply.github.com>
2025-07-08 21:54:09 +0200

    Update Product.java

--- src/main/java/org/example/model/Product.java
+++ src/main/java/org/example/model/Product.java
@@ -2,10 +2,18 @@ package org.example.model;
 
 public class Product {
     public int id;
-    public String title;
+    public String name;
+    public String price;
+    public String brand;
+    public String description;
+    public String category;
 
-    public Product(int id, String title) {
+    public Product(int id, String name, String price, String brand, String description, String category) {
         this.id = id;
-        this.title = title;
+        this.name = name;
+        this.price = price;
+        this.brand = brand;
+        this.description = description;
+        this.category = category;
     }
 }
